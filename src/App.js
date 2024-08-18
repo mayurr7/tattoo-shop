b889f140b7fa17527c0f9b0f63fb08e9516a181e
Mayur Tekale <mayurtekale7@gmail.com>
2024-08-18 16:45:02 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
       <Contact />
       <Footer />
       <Copyright />
-      {/* <div className='h-[4000px]'></div> */}
+      <div className='h-[4000px]'></div>
     </div>
   );
 };
