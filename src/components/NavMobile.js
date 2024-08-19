99b71b2b274fad179d04409a3520da2794e4a6bb
Mayur Tekale <mayurtekale7@gmail.com>
2024-08-19 18:48:18 +0530

    menu icon created for mobile

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,7 +1,28 @@
 import React from 'react';
+// import nav sdata
+import { navData } from "../data";
+// import components
+import Socials from "./Socials";
 
 const NavMobile = () => {
-  return <div>NavMobile</div>;
+  const {items} = navData;
+  return <nav className='w-full h-full flex flex-col justify-evenly overflow-hidden'>
+    <ul className='flex flex-col justify-center items-center gap-y-6 py-6 mb-8 '>
+      {items.map((item, index) => {
+        return (
+          <li key ={index}>
+              <a className='text-2xl font-primary uppercase' href={item.href}>
+                {item.name}
+                </a>
+          </li>
+        );
+      })}
+    </ul>
+
+    <div className='text-2xl'>
+      <Socials />
+    </div>
+    </nav>;
 };
 
 export default NavMobile;
