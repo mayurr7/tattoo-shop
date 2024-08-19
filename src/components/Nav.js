2e3d2bed9993ddf2c6562e4788081b0f28243e09
Mayur Tekale <mayurtekale7@gmail.com>
2024-08-19 18:47:58 +0530

    nav bar created

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,24 @@
 import React from 'react';
+// import nav data
+import { navData } from '../data';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  // destructor nav data
+  const {items} = navData;
+
+  return(
+     <nav>
+    <ul className='flex gap-x-[58px]'>
+      {items.map((item, index)=> {
+        return(
+           <li key={index}> 
+           <a className='link hover:border-b-2 hover:border-dark transition duration-300' href= {item.href}>{item.name}</a>
+        </li>
+        );
+      })}
+    </ul>
+  </nav>
+  );
 };
 
 export default Nav;
