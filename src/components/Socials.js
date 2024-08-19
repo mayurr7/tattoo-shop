cd90dd1bb693b0ec56766991938f52d0082b2364
Mayur Tekale <mayurtekale7@gmail.com>
2024-08-19 18:48:33 +0530

    social media logo

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,23 @@
 import React from 'react';
+// import social data
+
+import {socialData} from '../data';
+
+
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+  <ul className='flex justify-center items-center gap-x-[30px]'>
+    {socialData.map((item, index) => {
+      return <li key = {index}>
+        <a href={item.href}>{item.icon}</a>
+      </li>
+    })
+
+    }
+  </ul>
+  );
+
 };
 
 export default Socials;
