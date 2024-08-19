71d65f28fdbf0ceb253a9b655e187db70ad805d5
Mayur Tekale <mayurtekale7@gmail.com>
2024-08-19 18:47:49 +0530

    added refrance file

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,15 @@ import React, {useState, useEffect} from 'react';
 // import headeer data
 import { headerData } from '../data';
 // imports components
-import Nav from './Nav';
+import Nav from "./Nav";
 import NavMobile from './NavMobile';
 import Socials from './Socials';
 
+
+
 // import icons
-import { TiThMenuOutline } from "react-icons/ti";
+
+import { TiThMenuOutline } from 'react-icons/ti';
 
 
 
@@ -19,37 +22,59 @@ const Header = () => {
   const [isActive,setIsActive] = useState(false);
 
   //nav mobile state
-  const [NavMobile,,setNavMobile] = useState(false);
+  let [navMobile,setNavMobile] = useState(false);
 
   // scroll event
   useEffect(() => {
     window.addEventListener('scroll',() => {
       window.scrollY > 50 ? setIsActive(true) : setIsActive(false);
-    })
-  })
+    });
+  });
 
-  return <header
-  className={`${
-    isActive ? " h-[100px] lg:h-[110px] shadow-lg" : ' h-[120px] lg:h-[150px]'} fixed left-0 right-0 top-0 z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}
+return(
+  <header
+    className={`${
+    isActive 
+    ? ' h-[100px] lg:h-[110px] shadow-lg'
+    : ' h-[120px] lg:h-[150px]'
+    } fixed bg-white left-0 right-0  z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}
   >
     <div className='flex justify-between items-center h-full pl-[50px] pr-[60px]'>
+
       {/* logo */}
       <a href='/'>
         <img className='w-[188px] h-[90px]' src={logo} alt='' />
       </a>
+
     {/* nav - initially hidden - show on desktop */}
-
     <div className='hidden xl:flex'>
-      <nav/>
+      <Nav/>
     </div>
+
     {/* nav menu btn - showing by default - hidden on desktop mode */}
-
-    <div>
-      
+    <div onClick={() => setNavMobile(!navMobile)}  className='xl:hidden absolute right-[5%] bg-dark text-white p-2 rounded-md cursor-pointer'>
+      <TiThMenuOutline className='text-3xl'/>
     </div>
 
+        {/* nav mobile showing by default hidden on desktop */}
+        <div 
+        className={`${navMobile ? 'max-h-full' : 'max-h-0'} 
+        ${isActive
+          ? 'top-[100px] lg:top-[110px]'
+          : 'top-[120px] lg:top-[150px]'
+        }
+        fixed bg-white w-full h-full left-0 -z-10 transition-all duration-300 `}> 
+          <NavMobile /> 
+         
+        </div>
+        {/* social icon hidden show on desktop */}
+        <div className='hidden xl:flex'>
+          <Socials />
+        </div>
     </div>
-  </header>;
+  </header>
+);
 };
+
 
 export default Header;
